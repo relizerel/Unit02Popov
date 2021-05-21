8b70a96021fcd809d5070279e84a737ca3d488e6
relizerel <1590753Aa>
2021-05-21 16:54:43 +0200

    Added solution for 5 tasks

--- src/java/by/epam/unit02/main/Task05.java
+++ src/java/by/epam/unit02/main/Task05.java
@@ -1,7 +1,19 @@
 package by.epam.unit02.main;
 
+import static java.lang.Math.*;
+
 public class Task05 {
     public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        Task05 tempObject = new Task05();
+        System.out.println(tempObject.function(x, y));
+    }
 
+    private double function(int x, int y){
+        double temp1 = sin(x) + cos(y);
+        double temp2 =cos(x) - sin(y);
+        double temp3 = tan(x*y);
+        return temp1 * temp2 / temp3;
     }
 }
