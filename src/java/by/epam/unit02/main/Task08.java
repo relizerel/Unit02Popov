76e91ae92a1adccd46570f08f9a9054bc546e9bd
relizerel <1590753Aa>
2021-05-23 14:05:03 +0200

    Added solution for 8 tasks

--- src/java/by/epam/unit02/main/Task08.java
+++ src/java/by/epam/unit02/main/Task08.java
@@ -2,8 +2,8 @@ package by.epam.unit02.main;
 
 public class Task08 {
     public static void main(String[] args) {
-        double x = -0;
-        double y = -2;
+        double x = 1;
+        double y = -2.7;
         Task08 tempObject = new Task08();
         System.out.println(tempObject.function(x, y));
     }
@@ -11,6 +11,6 @@ public class Task08 {
     private boolean function(double x, double y) {
         if (((x >= -0.5) & (x <= 0.5)) & ((y >= -2.5) & (y <= -1.5))) {
             return false;
-        } else return (x >= -4) && (x <= 4) && (y >= -3) && (y <= 4);
+        } else return (x >= -4) & (x <= 4) & (y >= -3) & (y <= 4);
     }
 }
