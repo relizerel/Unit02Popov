154b706ee57a6086365b05ab005d3beedcb4a247
relizerel <1590753Aa>
2021-05-23 13:15:47 +0200

    Added solution for 6 tasks

--- src/java/by/epam/unit02/main/Task06.java
+++ src/java/by/epam/unit02/main/Task06.java
@@ -2,6 +2,14 @@ package by.epam.unit02.main;
 
 public class Task06 {
     public static void main(String[] args) {
-// ой-ой-ой
+        int m = 25;
+        int n = 5;
+        Task06 tempObject = new Task06();
+        tempObject.function(m, n);
+    }
+
+    private void function(int m, int n){
+        System.out.println("Старшая цифра дробной части: " + ((m*10)/n)%10 + "\n" +
+                "Младшая цифра целой части: " + (m/n)%10);
     }
 }
