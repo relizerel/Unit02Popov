34cfd46b5efd23475c5d49b31e1f3e3e90b45bdf
relizerel <1590753Aa>
2021-05-23 13:26:44 +0200

    Added solution for 7 tasks

--- src/java/by/epam/unit02/main/Task07.java
+++ src/java/by/epam/unit02/main/Task07.java
@@ -2,6 +2,26 @@ package by.epam.unit02.main;
 
 public class Task07 {
     public static void main(String[] args) {
-// не, я так не играю :(
+        int firstNumber = 0;
+        int secondNumber = 5;
+        int thirdNumber = -2;
+        Task07 tempObject = new Task07();
+        tempObject.function(firstNumber);
+        tempObject.function(secondNumber);
+        tempObject.function(thirdNumber);
     }
+
+    private void function(int someNumber) {
+        if (someNumber == 0){
+            System.out.println("Данное число рано 0");
+        } else if (someNumber > 0) {
+            System.out.println("Положительное число, возведенное в квадрат: " + squaredNumber(someNumber));
+        } else {
+            System.out.println("Отрицательное чило, возведенное в 4-ю степень: " + squaredNumber(squaredNumber(someNumber)));
+        }
+    }
+
+    private double squaredNumber(double someNumber){
+        return someNumber * someNumber;
+    } // Игрался с мелкими оптимизациями, смотрел на разницу с методом из Math
 }
