106598ce4d10b2ba51235a8f56da2a92fb579eba
relizerel <1590753Aa>
2021-05-23 14:17:11 +0200

    Added solution for 9 tasks

--- src/java/by/epam/unit02/main/Task09.java
+++ src/java/by/epam/unit02/main/Task09.java
@@ -2,6 +2,20 @@ package by.epam.unit02.main;
 
 public class Task09 {
     public static void main(String[] args) {
+        int x = -3;
+        Task09 tempObject = new Task09();
+        tempObject.function(x);
+    }
 
+    private void function(int x){
+        if (x <= -3){
+            System.out.println(9);
+        } else if (x > 3) {
+            System.out.println(formula(x));
+        }
+    }
+
+    private double formula(double x){
+        return 1 / (x * x + 1);
     }
 }
